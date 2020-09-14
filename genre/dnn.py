a207e3da48a4a9c9bd74caea38340dab5072a4d2
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-09-14 20:25:03 +0800

    Refactor neural network models

--- genre/dnn.py
+++ genre/dnn.py
@@ -32,37 +32,36 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=32):
 #   pass
 
 
-DATA_FILE_PATH = "data/data_clean.csv"
+DATA_FILE_PATH = "../data/data_clean.csv"
 SELECT_COLUMNS = ['Danceability', 'Energy', 'Key', 'Loudness', 'Mode', 'Speechness', 'Acousticness', 'Instrumentalness',
                   'Liveness', 'Valence', 'Tempo', 'Genre']
-FEATURE_COLUMNS = ['Danceability', 'Energy', 'Key', 'Loudness', 'Mode', 'Speechness', 'Acousticness',
-                   'Instrumentalness', 'Liveness', 'Valence', 'Tempo']
 
-data = pd.read_csv(DATA_FILE_PATH)[FEATURE_COLUMNS]
-label_vocab = data['Genre'].unique()
+data = pd.read_csv(DATA_FILE_PATH)[SELECT_COLUMNS]
+label_vocab = data['Genre'].unique().tolist()
 
 train, test = train_test_split(data, test_size=0.2)
-train, val = train_test_split(train, test_size=0.2)
 print(len(train), 'train examples')
-print(len(val), 'validation examples')
 print(len(test), 'test examples')
+print(len(label_vocab), 'classes')
 
-train_ds = df_to_dataset(train)
-val_ds = df_to_dataset(val, shuffle=False)
-test_ds = df_to_dataset(test, shuffle=False)
+# train_ds = df_to_dataset(train)
+# test_ds = df_to_dataset(test, shuffle=False)
 
 feature_columns = []
-for feature in FEATURE_COLUMNS:
-    feature_columns.append(feature_column.numeric_column(feature))
+for feature in SELECT_COLUMNS:
+    if feature != 'Genre':
+        feature_columns.append(feature_column.numeric_column(feature))
 
-feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+feature_layer = layers.DenseFeatures(feature_columns)
 
 classifier = tf.estimator.DNNClassifier(
     feature_columns=feature_columns,
-    hidden_units=[1024, 512, 256],
+    hidden_units=[128, 128],
     n_classes=len(label_vocab),
-    label_vocabulary=label_vocab,
-    optimizer=tf.compat.v1.train.ProximalAdagradOptimizer(
-        learning_rate=0.1,
-        l1_regularization_strength=0.001
-    ))
+    label_vocabulary=label_vocab)
+
+classifier.train(input_fn=lambda: df_to_dataset(train, batch_size=5))
+metrics = classifier.evaluate(input_fn=lambda: df_to_dataset(test, shuffle=False, batch_size=5))
+# predictions = classifier.predict(input_fn=input_fn_predict)
+
+print(metrics)
