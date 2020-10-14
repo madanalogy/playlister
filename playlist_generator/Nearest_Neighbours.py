b511771488c4003302516347f577dd49f02d9cc3
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-10-14 10:55:59 +0800

    Refactor NN model for interoperability

--- playlist_generator/Nearest_Neighbours.py
+++ playlist_generator/Nearest_Neighbours.py
@@ -9,10 +9,13 @@ from sklearn.decomposition import PCA
 
 major_genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
 
-features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness',
+            'liveness', 'valence', 'tempo']
+
 
 def filter(data, column, value):
     return data.loc[[(value in row) for row in data[column]]]
+
 
 def display_songs_1(songs, genre, valence):
     message = f'Genre: {genre}\n'
@@ -27,6 +30,7 @@ def display_songs_1(songs, genre, valence):
 
     print(message)
 
+
 def display_songs_2(songs):
     main_song = songs[0]
     message = f'Song: {main_song["name"]} by {main_song.artists}\n'
@@ -40,12 +44,13 @@ def display_songs_2(songs):
 
     print(message)
 
+
 def find_songs_by_genre(song_index, n=10):
     x = data[features]
     x_scaled = StandardScaler().fit_transform(x)
     song_scaled = x_scaled[song_index]
 
-    nbrs = NearestNeighbors(n_neighbors = n + 1, algorithm = 'ball_tree').fit(x_scaled)
+    nbrs = NearestNeighbors(n_neighbors=n + 1, algorithm='ball_tree').fit(x_scaled)
     distances, indices = nbrs.kneighbors(np.array([song_scaled]))
 
     songs = []
@@ -53,6 +58,7 @@ def find_songs_by_genre(song_index, n=10):
         songs.append(data.iloc[song_id])
 
     return songs
+
 
 def find_songs_by_genre_PCA(song_index, n=10, components=7):
     x = data[features]
@@ -62,7 +68,7 @@ def find_songs_by_genre_PCA(song_index, n=10, components=7):
     principalComponents = pca.fit_transform(x_scaled)
     song_scaled = principalComponents[song_index]
 
-    nbrs = NearestNeighbors(n_neighbors = n + 1, algorithm = 'ball_tree').fit(principalComponents)
+    nbrs = NearestNeighbors(n_neighbors=n + 1, algorithm='ball_tree').fit(principalComponents)
     distances, indices = nbrs.kneighbors(np.array([song_scaled]))
 
     songs = []
@@ -70,6 +76,7 @@ def find_songs_by_genre_PCA(song_index, n=10, components=7):
         songs.append(data.iloc[song_id])
 
     return songs
+
 
 def find_songs_by_valence(genre, valence, n=10):
     data = pd.read_csv('popular_' + genre + '_songs.csv')
@@ -79,30 +86,32 @@ def find_songs_by_valence(genre, valence, n=10):
 
     data['diff'] = diff
 
-    data = data.sort_values(['diff', 'popularity'], ascending = [True, False])
+    data = data.sort_values(['diff', 'popularity'], ascending=[True, False])
     songs = data.head(n)
     return songs
 
-workflow = int(input ("Choose a workflow: "))
-playlist_length = int(input ("Enter the length of the playlist (must be less than 3000): "))
 
-if workflow == 1:
-    # Workflow 1
-    genre = input ("Input a genre: ")
-    valence = float(input ("Input a valence: "))
+if __name__ == '__main__':
+    workflow = int(input("Choose a workflow: "))
+    playlist_length = int(input("Enter the length of the playlist (must be less than 3000): "))
 
-    songs = find_songs_by_valence(genre, valence, n=playlist_length)
-    display_songs_1(songs, genre, valence)
+    if workflow == 1:
+        # Workflow 1
+        genre = input("Input a genre: ")
+        valence = float(input("Input a valence: "))
 
-elif workflow == 2:
-    # Workflow 2
-    song_id = input ("Input a Spotify ID: ")
+        songs = find_songs_by_valence(genre, valence, n=playlist_length)
+        display_songs_1(songs, genre, valence)
 
-    data = pd.read_csv('songs_with_genres.csv')
-    song_index = data.loc[data['id'] == song_id].index[0]
+    elif workflow == 2:
+        # Workflow 2
+        song_id = input("Input a Spotify ID: ")
 
-    my_songs = find_songs_by_genre(song_index, n=playlist_length)
-    my_songs_PCA = find_songs_by_genre_PCA(song_index, n=playlist_length)
+        data = pd.read_csv('songs_with_genres.csv')
+        song_index = data.loc[data['id'] == song_id].index[0]
 
-    display_songs_2(my_songs)
-    display_songs_2(my_songs_PCA)
+        my_songs = find_songs_by_genre(song_index, n=playlist_length)
+        my_songs_PCA = find_songs_by_genre_PCA(song_index, n=playlist_length)
+
+        display_songs_2(my_songs)
+        display_songs_2(my_songs_PCA)
